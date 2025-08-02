192461a66b386d721a2096d271bd2c8183bb9a55
thakshila-dilrukshi <thakshiladilrukshi22ah@gmail.com>
2025-08-02 11:25:23 +0530

    Implement Logout Functionality (#6)

--- src/main/java/com/icbt/servlet/LoginServlet.java
+++ src/main/java/com/icbt/servlet/LoginServlet.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     private UserService userService;
 
+
     @Override
     public void init(){userService=new UserService();}
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
